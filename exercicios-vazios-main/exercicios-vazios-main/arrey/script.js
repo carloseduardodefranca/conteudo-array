b3a51c540ad1bce13df6765bcac5c42f60b7588e
Carlos-França <carlos705ef@gmail.com>
2025-04-08 21:17:50 -0300

    Add files via upload

---
+++ exercicios-vazios-main/exercicios-vazios-main/arrey/script.js
@@ -0,0 +1,5 @@
+/* var carros =["ranger" , "frontier" , "L200" ]
+
+alert( carro[0]) */
+
+ 
---
+++ exercicios-vazios-main/exercicios-vazios-main/objetos/script.js
@@ -0,0 +1,25 @@
+/* var pessoa = {
+    nome: "carlos",
+    idade: 20,
+    bigode: true,
+    altura: 1.75
+}
+alert(`nome ${pessoa.nome} 
+    idade ${pessoa.idade}
+    bigode ${pessoa.bigode}
+    altura ${pessoa.altura}`) */
+
+    var carro ={
+        marca: "ford",
+        modelo: "Ranger",
+        portas: 4,
+        cor: "branco",
+        preco: 1890000
+
+    }
+    alert(`carro 
+        marca: ${carro.marca}
+        modelo: ${carro.modelo}
+        portas: ${carro.portas}
+        cor: ${carro.cor}
+        preco: ${carro.preco}`)
